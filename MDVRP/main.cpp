c56f8d5bcb7d18dc95a605e1fb7a1503b181a70f
Agnar22 <agnar.bjornstad@gmail.com>
2021-03-04 16:07:17 +0100

    Load problem.

--- MDVRP/main.cpp
+++ MDVRP/main.cpp
@@ -4,6 +4,9 @@
 #include <fstream>
 #include <sstream>
 #include <regex>
+#include <utility>
+#include <math.h>
+#include <limits>
 
 // TODO: representation of individuals.
 // Ideas:
@@ -22,7 +25,51 @@ class GA {
 };
 
 class Problem {
+    private:
+        int num_customers, num_depots;
+        std::vector<double> max_length, max_load;
+        std::vector<std::pair<double, double>> positions;
+        std::vector<double> cust_serv_dur, cust_demand;
+        std::vector<std::vector<double>> distances;
 
+        void calculate_distances() {
+            distances.resize(positions.size(), std::vector<double>(positions.size()));
+            for (int from=0; from<positions.size(); ++from) {
+                for (int to=0; to<from; ++to) {
+                    std::cout << "from " << from << " to " << to << std::endl;
+                    double dist = euclidian_dist(positions[from], positions[to]);
+                    distances[from][to]=dist;
+                    distances[to][from]=dist;
+                }
+            }
+        }
+
+        double euclidian_dist(std::pair<double, double> p1, std::pair<double, double> p2) {
+            return sqrt(pow(p1.first-p2.first, 2)+pow(p1.second-p2.second, 2));
+        }
+
+    public:
+
+    Problem(
+        std::vector<double> max_length,
+        std::vector<double> max_load,
+        std::vector<std::pair<double, double>> positions,
+        std::vector<double> cust_serv_dur,
+        std::vector<double> cust_demand
+    ) {
+        this->max_length = max_length;
+        this->max_load = max_load;
+        this->positions = positions;
+        this->cust_serv_dur = cust_serv_dur;
+
+        std::cout << "Calculating distance." << std::endl;
+        calculate_distances();
+        std::cout << "Calculated distance." << std::endl;
+    }
+
+    double get_distance(int from, int to) {
+        return distances[from][to];
+    }
 };
 
 namespace file {
@@ -30,7 +77,7 @@ namespace file {
         std::ifstream fs(file_name);
         std::vector<std::vector<double>> file;
         std::string line;
-        std::cout << "Opening file" << std::endl;
+        std::cout << "Opening file " << file_name << std::endl;
         while (std::getline(fs, line)) {
             // Remove leading and trailing spaces.
             line = std::regex_replace(line, std::regex("^ +| +$|( ) +"), "$1");
@@ -44,6 +91,33 @@ namespace file {
         }
         fs.close();
         return file;
+    }
+
+    Problem load_problem(std::string file_name) {
+        std::vector<std::vector<double>> prob = read_flat(file_name);
+
+        int max_vhcl_pr_depot = prob[0][0];
+        int num_cust = prob[0][1];
+        int num_depots = prob[0][2];
+
+        std::vector<double> max_length;
+        std::vector<double> max_load;
+        for (int row = 1; row < num_depots+1; ++row) {
+            max_length.push_back(prob[row][0]);
+            max_load.push_back(prob[row][1]);
+        }
+
+        std::vector<std::pair<double, double>> positions;
+        std::vector<double> cust_serv_dur, cust_demand;
+        for (int row = num_depots+1; row < num_cust+num_depots+1; ++row) {
+            positions.push_back(std::make_pair(prob[row][1], prob[row][2]));
+            cust_serv_dur.push_back(prob[row][3]);
+            cust_demand.push_back(prob[row][4]);
+        }
+
+        std::cout << "Finished cleaning data." << std::endl;
+
+        return Problem(max_length, max_load, positions, cust_serv_dur, cust_demand);
     }
 }
 
@@ -59,4 +133,9 @@ int main() {
             std::cout << val << " ";
         std::cout << std::endl;
     }
+
+    Problem curr_prob = file::load_problem(file_name);
+    std::cout.precision(17);
+    std::cout << "distance " <<  curr_prob.get_distance(1,2) << std::endl;
+    std::cout << "distance " <<  curr_prob.get_distance(3,2) << std::endl;
 }
