0c8d56a7634b8c03b4fccef884f680e6a72dcd46
Agnar22 <agnar.bjornstad@gmail.com>
2021-03-04 15:02:43 +0100

    General setup of main.

---
+++ MDVRP/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+// TODO: representation of individuals.
+// Ideas:
+//  - initialize customer to closest depot (obs. be carefull of edgecase where it is not illegal to have all at for one depot or similar)
+//  - elitism
+//  - variation operators with different kinds of "jumps" - small perfections and larger jumps
+//  - niching
+
+
+struct Individual {
+
+};
+
+class GA {
+
+};
+
+class Problem {
+
+};
+
+
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+}
