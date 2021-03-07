387f7b9b55284bbd553033458f4eecf8dd43f65a
Agnar22 <agnar.bjornstad@gmail.com>
2021-03-07 17:15:23 +0100

    Implement forward and backward for ordering trips for a depot.

--- MDVRP/main.cpp
+++ MDVRP/main.cpp
@@ -89,67 +89,17 @@ class Problem {
 
     int get_vhcl_pr_depot(){return vhcl_pr_depot;}
 
+    double get_customer_load(int cust){return cust_demand[cust];}
+
     std::vector<double> get_max_length(){return max_length;}
 };
-
-namespace file {
-    std::vector<std::vector<double>> read_flat(std::string file_name) {
-        std::ifstream fs(file_name);
-        std::vector<std::vector<double>> file;
-        std::string line;
-        std::cout << "Opening file " << file_name << std::endl;
-        while (std::getline(fs, line)) {
-            // Remove leading and trailing spaces.
-            line = std::regex_replace(line, std::regex("^ +| +$|( ) +"), "$1");
-            std::istringstream s(line);
-            std::string val;
-            std::vector<double> row;
-            while (std::getline(s, val, ' '))
-                if (val.compare(" ")!=0)
-                    row.push_back(std::stod(val));
-            file.push_back(row);
-        }
-        fs.close();
-        return file;
-    }
-
-    Problem load_problem(std::string file_name) {
-        std::vector<std::vector<double>> prob = read_flat(file_name);
-
-        int max_vhcl_pr_depot = (int)prob[0][0];
-        int num_cust = (int)prob[0][1];
-        int num_depots = (int)prob[0][2];
-
-        std::vector<double> max_length;
-        std::vector<double> max_load;
-        for (int row = 1; row < num_depots+1; ++row) {
-            max_length.push_back(prob[row][0]);
-            max_load.push_back(prob[row][1]);
-        }
-
-        std::vector<std::pair<double, double>> positions;
-        std::vector<double> cust_serv_dur, cust_demand;
-        for (int row = num_depots+1; row < num_cust+num_depots+1; ++row) {
-            positions.push_back(std::make_pair(prob[row][1], prob[row][2]));
-            cust_serv_dur.push_back(prob[row][3]);
-            cust_demand.push_back(prob[row][4]);
-        }
-        for (int row = num_cust+num_depots+1; row < num_cust+2*num_depots+1; ++row) {
-            positions.push_back(std::make_pair(prob[row][1], prob[row][2]));
-        }
-
-
-        std::cout << "Finished cleaning data." << std::endl;
-
-        return Problem(num_cust, num_depots, max_vhcl_pr_depot, max_length, max_load, positions, cust_serv_dur, cust_demand);
-    }
-}
-
 
 struct Individual {
     std::vector<std::vector<int>> chromosomes;
     std::vector<std::vector<int>> cust_on_depots;
-    std::vector<std::vector<int>> chromosome_trip_ends;
+    std::vector<std::vector<std::vector<int>>> chromosome_trips;
+    std::vector<std::vector<double>> trip_dists;
+    std::vector<std::vector<int>> trip_loads;
     double fitness;
 
     Individual (Problem pr) {
@@ -168,19 +118,23 @@ struct Individual {
         // Assign customers to depots.
         cust_on_depots = assign_customers_to_depots(pr);
         // Assign routes to depots.
+        chromosome_trips.resize(chromosomes.size());
+        trip_dists.resize(chromosomes.size());
+        trip_loads.resize(chromosomes.size());
         for (int depot=0; depot<pr.get_num_depots(); ++depot) {
-            chromosomes[depot] = order_depot(depot, cust_on_depots[depot], pr);
+            setup_trips(depot, cust_on_depots[depot], pr);
         }
     }
 
     std::vector<std::vector<int>> assign_customers_to_depots(Problem &pr) {
         // Stochastically assign customers to depots based on inverse distanse to depot.
+        // TODO: Implement deterministically.
         std::vector<std::vector<int>> customers_on_depot(pr.get_num_depots());
         for (int cust=0; cust<pr.get_num_customers(); ++cust) {
             std::vector<std::vector<double>> depot_dist = pr.get_depot_distances();
             std::vector<double> depot_prob;
             for (double dist:depot_dist[cust]) {
-                depot_prob.push_back(1/std::pow(dist, 3));
+                depot_prob.push_back(1/std::pow(dist, 10));
             }
             double sum=0;
             std::for_each(depot_prob.begin(), depot_prob.end(), [&] (double n) {sum += n;});
@@ -201,16 +155,22 @@ struct Individual {
     }
 
     template <class T>
-    static std::vector<T> get_subset(std::vector<T> &vals, std::vector<int> idxs) {
+    static std::vector<T> get_subset(std::vector<T> &vals, std::vector<int> &idxs) {
+        /**
+         * Returns a subset of vals corresponding to the values of idxs.
+         */
         std::vector<T> subset(idxs.size());
         for (int i=0; i<idxs.size(); ++i)
-            subset[i]=vals[i];
+            subset[i]=vals[idxs[i]];
         return subset;
     }
 
-    std::vector<int> order_depot(int depot_num, std::vector<int> customers, Problem &pr) {
+    void setup_trips_forward(int depot_num, std::vector<int> customers, Problem &pr) {
         std::vector<int> order;
+        std::vector<int> cur_trip;
         double trip_dist = 0;
+        double trip_load = 0;
+        double max_trip_dist = pr.get_max_length()[depot_num];
         int cur_cust = pr.get_num_customers() + depot_num;
         int num_vhcl = 1;
         std::vector<std::vector<double>> distances = pr.get_distances();
@@ -218,30 +178,101 @@ struct Individual {
             std::vector<double> cust_distances = Individual::get_subset<double>(pr.get_distances()[cur_cust], customers);
             int closest_cust_pos = std::min_element(cust_distances.begin(), cust_distances.end()) - cust_distances.begin();
             int closest_cust = customers[closest_cust_pos];
+            double closest_cust_dist = distances[cur_cust][closest_cust];
+            double trip_and_ret_dist = trip_dist+closest_cust_dist+distances[closest_cust][pr.get_num_customers()+depot_num];
+            double closest_cust_depot_dist = distances[closest_cust][pr.get_num_customers()+depot_num];
             // Check that the current trip is not too long.
-            if ((pr.get_max_length()[depot_num]==0) || trip_dist+distances[cur_cust][closest_cust]+distances[closest_cust][pr.get_num_customers()+depot_num] <= pr.get_max_length()[depot_num]){
+            if (/*closest_cust_dist <= closest_cust_depot_dist && */(trip_and_ret_dist <= max_trip_dist || max_trip_dist==0)){
                 order.push_back(closest_cust);
+                cur_trip.push_back(closest_cust);
                 trip_dist += distances[cur_cust][closest_cust];
+                trip_load += pr.get_customer_load(closest_cust);
                 customers.erase(customers.begin()+closest_cust_pos);
+                cur_cust = closest_cust;
             } else if (trip_dist==0) {
                 std::cout << "Trip could not be created as first point is too far away: " << closest_cust << " with distance "  << distances[closest_cust][pr.get_num_customers()+depot_num] << std::endl;
             } else {
                 cur_cust = pr.get_num_customers() + depot_num;
+
+                chromosome_trips[depot_num].push_back(cur_trip);
+                cur_trip.clear();
+                double ret_dist = distances[cur_cust][pr.get_num_customers()+depot_num];
+                trip_dist+= ret_dist;
+                trip_dists[depot_num].push_back(trip_dist);
                 trip_dist = 0;
+
+                trip_loads[depot_num].push_back(trip_load);
+                trip_load = 0;
+
                 ++num_vhcl;
+            }
+            if (trip_dist>0 && customers.size()==0) {
+                chromosome_trips[depot_num].push_back(cur_trip);
+                cur_trip.clear();
+                double ret_dist = distances[cur_cust][pr.get_num_customers()+depot_num];
+                trip_dist+= ret_dist;
+                trip_dists[depot_num].push_back(trip_dist);
+                trip_dist = 0;
+
+                trip_loads[depot_num].push_back(trip_load);
+                trip_load = 0;
             }
         }
         if (num_vhcl>pr.get_vhcl_pr_depot()) {
             std::cout << "Too many vehicles for depot: " << depot_num << " with "  << num_vhcl << " vehicles." << std::endl;
         }
-        return order;
     }
 
-    void find_direct_mapping() {
-        chromosome_trip_ends.resize(chromosomes.resize());
-        for (int num_chrm; num_chrm<chromosomes.size(); ++num_chrm) {
+    void setup_trips_backward(int depot_num, std::vector<int> customers, Problem &pr) {
+        std::vector<std::vector<double>> distances = pr.get_distances();
+        // Is it better that the back is a new trip?
+        if (chromosome_trips[depot_num].size()<pr.get_vhcl_pr_depot()) {
+            int l_cust_lt = chromosome_trips[depot_num].back().back();
+            int sl_cust_lt = chromosome_trips[depot_num].back()[chromosome_trips[depot_num].back().size()-2];
+            double dist_l_dep_pt = distances[l_cust_lt][pr.get_num_customers()+depot_num];
+            double dist_sl_l_pt = distances[sl_cust_lt][l_cust_lt];
+            double dist_sl_dep_pt = distances[sl_cust_lt][pr.get_num_customers()+depot_num];
 
+            if (2*dist_l_dep_pt+dist_sl_dep_pt<dist_sl_l_pt+dist_l_dep_pt) {
+                chromosome_trips[depot_num].back().erase(chromosome_trips[depot_num].back().end()-1);
+                chromosome_trips[depot_num].push_back(std::vector<int>{l_cust_lt});
+                trip_dists[depot_num].back()+=dist_sl_dep_pt-dist_sl_l_pt-dist_l_dep_pt;
+                trip_dists[depot_num].push_back(2*dist_l_dep_pt);
+            }
         }
+        for (int trip_num=chromosome_trips[depot_num].size()-1; trip_num>0; --trip_num) {
+            /**
+             * pt=previous trip, ct=current trip, l=last, sl=second last, f=first
+             */
+            // TODO: Edge case if pt consist of only one node.
+            int l_cust_pt = chromosome_trips[depot_num][trip_num-1].back();
+            int sl_cust_pt = chromosome_trips[depot_num][trip_num-1][chromosome_trips[depot_num][trip_num-1].size()-2];
+            int f_cust_ct = chromosome_trips[depot_num][trip_num][0];
+            double dist_l_dep_pt = distances[l_cust_pt][pr.get_num_customers()+depot_num];
+            double dist_sl_l_pt = distances[sl_cust_pt][l_cust_pt];
+            double dist_sl_dep_pt = distances[sl_cust_pt][pr.get_num_customers()+depot_num];
+            double dist_l_pt_f_ct = distances[l_cust_pt][f_cust_ct];
+            double dist_dep_f_ct = distances[pr.get_num_customers()+depot_num][f_cust_ct];
+
+            if (dist_sl_dep_pt+dist_l_pt_f_ct<dist_sl_l_pt+dist_dep_f_ct) {
+                chromosome_trips[depot_num][trip_num-1].erase(chromosome_trips[depot_num][trip_num-1].end()-1);
+                chromosome_trips[depot_num][trip_num].insert(chromosome_trips[depot_num][trip_num].begin(), l_cust_pt);
+                trip_dists[depot_num][trip_num-1]+=dist_sl_dep_pt-dist_sl_l_pt-dist_l_dep_pt;
+                trip_dists[depot_num][trip_num]+=dist_l_dep_pt+dist_l_pt_f_ct-dist_dep_f_ct;
+                ++trip_num;
+            }
+        }
+
+    }
+
+    void setup_trips(int depot_num, std::vector<int> customers, Problem &pr) {
+        /**
+         * Initializes the trips for a depot by greedily adding the customer that is closest.
+         * A trip is ended if it is shorter to start a new or the maximum trip distance is exceeded.
+         * The trip distance and trip load is calculated for each trip.
+         */
+        setup_trips_forward(depot_num, customers, pr);
+        setup_trips_backward(depot_num, customers, pr);
     }
 };
 
